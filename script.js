98439b0b3741eacca122a5dd142c56a16271265e
e <sarahbezerra2013@gmail.com>
2021-11-12 12:02:37 -0300

    Mensagens padrão sendo recebidas no chat

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // LOGIN
-/* */
 function hideLogin(){
     const login = document.querySelector(".login");
     login.classList.add("hidden");
@@ -8,13 +7,14 @@ function hideLogin(){
 function verificationSuccess(resposta){
     console.log(resposta)
     hideLogin()
+    // add function carregar mensagens
 }
 
-function verificationError(resposta) {
-    //console.log(erro.response.status)
+function verificationError(erro) {
+    console.log(erro.response.status)
 }
 
-function verifyUser() { // verificar se usuário é válido
+function verifyUser() { // verifica se usuário é válido
     const nameInput = document.querySelector(".input-name").value;
 
     const promise = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/participants", {name: nameInput})
@@ -23,18 +23,42 @@ function verifyUser() { // verificar se usuário é válido
 }
 
 
-/*
+
 // CARREGAR MENSAGENS
 const promise = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/messages");
 promise.then(verifyMessage)
 
 function verifyMessage(resposta){
-    console.log(resposta.data)
+    const server_data = resposta.data;   
+    console.log(server_data)
+
+    for(let i=0; i<server_data.length; i++){
+        if(server_data[i].type === "message" && server_data[i].to === "Todos"){
+            const posts = document.querySelector(".posts");
+            posts.innerHTML += `<div class="message">
+                                    <span class="hour">(${server_data[i].time})</span>
+                                    <span class="info"> ${server_data[i].from} <span class="normal"> para </span> ${server_data[i].to}: </span>
+                                    <span class="contents"> ${server_data[i].text} </span> 
+                                </div>`
+        }
+        
+    }
+
+    const message_hour = document.querySelector(".message .hour");
+    //message_hour.innerHTML = resposta.data[0].time;
+    //console.log(message_hour)
+
+    const message_info = document.querySelector(".message .info");
+    //console.log(message_info)
+
+    const message_contents = document.querySelector(".message .contents");
+    //console.log(message_contents)
+
 }
 
 
 
-
+/*
 
 
 
